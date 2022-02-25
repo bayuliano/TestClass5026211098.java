462db163d352fcf49de4d6f4240ca146b7fac20a
Bayu Liano Leader Habibullah <95364030+bayuliano@users.noreply.github.com>
2022-02-25 12:05:34 +0700

    TestClass5026211098.java
    Computes the hypotenuse length of a triangle given its side lengths.

---
+++ TestClass5026211098.java
@@ -0,0 +1,10 @@
+package testclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  double Sdepan=24, Ssamping=48, Smiring;
+    Smiring = Math.sqrt((Sdepan*Sdepan)+(Ssamping*Ssamping));
+    System.out.println("panjang sisi miring nya adalah = " + Smiring);
+    }
+}
